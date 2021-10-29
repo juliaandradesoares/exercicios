89d781d2e48cea583078751e27f080c61fa2ae18
julia <juliaandradesoares@gmail.com>
2021-10-29 12:37:29 -0300

    Primeiro exercicio

---
+++ modulo-1/bloco-4/dia-1/scripts.js
@@ -0,0 +1,19 @@
+const myName = "Júlia";
+
+const birthCity = "São Gonçalo";
+
+let birthYear = 1994;
+
+console.log(myName);
+
+console.log(birthCity);
+
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Niterói";
+
+console.log(birthCity);
---
+++ modulo-1/bloco-4/dia-1/scripts2.js
@@ -0,0 +1,22 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+let base = 5;
+let heigth = 8;
+
+let area = base * heigth;
+console.log(area);
+
+let perimeter = base + base + heigth + heigth;
+console.log(perimeter);
