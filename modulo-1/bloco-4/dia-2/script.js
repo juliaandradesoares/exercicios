2be4fcd7fac6651780b9e6d04d6536347a6ed83c
julia <juliaandradesoares@gmail.com>
2021-11-12 10:56:13 -0300

    Iniciando exercicios

---
+++ modulo-1/bloco-4/dia-2/script.js
@@ -0,0 +1,39 @@
+/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa no fim do array
+console.log(tasksList);
+
+tasksList.unshift("Acordar"); // adiciona mais uma tarefa no início do array
+
+console.log(tasksList);
+
+tasksList.pop();  // remove a última tarefa
+console.log(tasksList);
+
+tasksList.shift();  // remove a última tarefa
+console.log(tasksList);*/
+
+// Exercícios
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+menu.push("Contato");
+
+console.log(menu);
