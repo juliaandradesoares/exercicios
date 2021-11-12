d85a9342df7571fb31985128ff436cc48a76f663
julia <juliaandradesoares@gmail.com>
2021-11-12 11:34:02 -0300

    Exercicios for/of

--- modulo-1/bloco-4/dia-2/script.js
+++ modulo-1/bloco-4/dia-2/script.js
@@ -45,3 +45,11 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for(let i = 0; i < groceryList.length; i += 1) {
     console.log(groceryList[i]);
 }
+
+// Exercícios for/of
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let nome of names) {
+    console.log("Boas-vindas, " + nome);
+}
