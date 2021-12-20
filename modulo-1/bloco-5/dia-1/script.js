60639836fab5ff92378b57bbfa9fd1ec3ec963b5
julia <juliaandradesoares@gmail.com>
2021-12-20 19:09:46 -0300

    Exerccios modulo 1, bloco 5, dia 1

---
+++ modulo-1/bloco-5/dia-1/script.js
@@ -0,0 +1,15 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'purple';
+
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'purple';
+
+document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'black';
+
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
+
+document.getElementById('footer-container').style.backgroundColor = 'green';
